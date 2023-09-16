21054cf5eafa14a5225bccdada1d94ac3ed25827
kn25ha01 <kn25ha01@gmail.com>
2023-09-17 00:39:23 +0900

    change how to use container for develop

--- docker_run.sh
+++ docker_run.sh
@@ -3,7 +3,7 @@ ENV=$1
 NAME=ts-learning
 
 if [ "$ENV" = "dev" ]; then
-    docker run --rm -it --publish=3000:3000 $NAME:$ENV
+    docker run -it --publish=3000:3000 --volume=${HOME}/work/typescript-learning:/myapp $NAME:$ENV
 elif [ "$ENV" = "prod" ]; then
     docker run --rm -it --publish=80:80 $NAME:$ENV
 else
